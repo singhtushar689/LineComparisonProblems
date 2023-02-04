3239482bf5c93d1ad4247219b8cf722705656ad0
singhtushar689 <singhtushar689@gmail.com>
2023-02-04 11:42:20 +0530

    [Tushar] add:Calculating Length of Line

---
+++ New folder/LineComparisonProblem/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisonProblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Length length_One = new Length(9,7,2,3);      //UC1
+            double length1 = length_One.Calculate();
+            Console.ReadLine();
+        }
+    }
+}
