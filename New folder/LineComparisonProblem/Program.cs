f8fc5da12776a0bb215dceb9f467c8f27db5d54c
singhtushar689 <singhtushar689@gmail.com>
2023-02-04 12:01:10 +0530

    [Tushar] add: Using Compare Method To check whether Lines are equal or not

--- New folder/LineComparisonProblem/Program.cs
+++ New folder/LineComparisonProblem/Program.cs
@@ -15,10 +15,23 @@ namespace LineComparisonProblem
 
             Length length_Two = new Length(3, 5, 2, 4);      //UC2
             double length2 = length_Two.Calculate();
-            if (length1.Equals(length2))
+            //if (length1.Equals(length2))
+            //    Console.WriteLine("Lines are Equal");
+            //else
+            //    Console.WriteLine("Lines are not Equal");
+
+            if (length1.CompareTo(length2) == 0)            //UC3
+            {
                 Console.WriteLine("Lines are Equal");
+            }
+            else if (length1.CompareTo(length2) < 0)
+            {
+                Console.WriteLine("Line2 is greater than Line1");
+            }
             else
-                Console.WriteLine("Lines are not Equal");
+            {
+                Console.WriteLine("Line1 is greater than Line2");
+            }
             Console.ReadLine();
         }
     }
