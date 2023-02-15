3d155d2c607f440f9e543f4311858d6431634c14
singhtushar689 <singhtushar689@gmail.com>
2023-02-15 10:59:17 +0530

    [Tushar] add: Making Object

---
+++ LineComparison/LineComparisonProblem/Length.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisonProblem
+{
+    public class Length
+    {
+        int lengthX, _lengthY, lenGthsX, lengthsY;
+        public Length(int lengthX, int lengthY, int lengthsX, int lengthsY)
+        {
+            this.lengthX = lengthX;               //These are different types to declare constructor
+            _lengthY = lengthY;
+            lenGthsX = lengthsX;
+            this.lengthsY = lengthsY;
+        }
+        public double Calculate()
+        {
+            double length = Math.Sqrt(Math.Pow((lenGthsX - lengthX), 2) + (Math.Pow((lengthsY - _lengthY), 2)));
+            Console.WriteLine(length);
+            return length;
+        }
+    }
+}
---
+++ LineComparison/LineComparisonProblem/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisonProblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Length length_One = new Length(4,7,8,9);      //UC1
+            double length1 = length_One.Calculate();
+
+            Length length_Two = new Length(3, 5, 2, 4);      //UC2
+            double length2 = length_Two.Calculate();
+            //if (length1.Equals(length2))
+            //    Console.WriteLine("Lines are Equal");
+            //else
+            //    Console.WriteLine("Lines are not Equal");
+
+            if (length1.CompareTo(length2) == 0)            //UC3
+            {
+                Console.WriteLine("Lines are Equal");
+            }
+            else if (length1.CompareTo(length2) < 0)
+            {
+                Console.WriteLine("Line2 is greater than Line1");
+            }
+            else
+            {
+                Console.WriteLine("Line1 is greater than Line2");
+            }
+            Console.ReadLine();
+        }
+    }
+}
